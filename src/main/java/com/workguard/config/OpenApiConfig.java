49dd922dcc65fe24a56226275a2908f21d487a36
MinJung Kim <alswnd0830@naver.com>
2026-08-28 02:27:34 +0900

    fix: fix Swagger servers base URL to relative for HTTPS mixed content

--- src/main/java/com/workguard/config/OpenApiConfig.java
+++ src/main/java/com/workguard/config/OpenApiConfig.java
@@ -5,8 +5,11 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
 
 @Configuration
 public class OpenApiConfig {
@@ -28,6 +31,7 @@ public class OpenApiConfig {
                         .title("WorkGuard API")
                         .description("WorkGuard 백엔드 API 문서 및 테스트 인터페이스")
                         .version("v1.0.0"))
+                .servers(List.of(new Server().url("/").description("Default Base URL")))
                 .addSecurityItem(securityRequirement)
                 .components(components);
     }
